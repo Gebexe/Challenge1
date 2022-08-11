880c4ca49e19a50d1d50ae5b19d0c9342213eb9f
Gebexe <84429662+Gebexe@users.noreply.github.com>
2022-08-11 12:52:07 -0300

    Test
    Completada primera consigna, segunda consigna quedo incompleta

---
+++ TEST/src/app/course/course.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../course';
+import { CourseService } from '../course.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+
+export class CourseComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  constructor(private courseService: CourseService) { 
+
+  }
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  getCourses(): void {
+    this.courseService.getCourses().subscribe(
+      courses => this.courses = courses 
+    );
+  }
+
+  delete(course: Course): void {
+    this.courseService.deleteCourse(course.id).subscribe(
+     a => this.courses = this.courses.filter(c => c !== course)
+    );
+  }
+
+  desarrollo(course:Course): void{
+    this.courseService.updateCourseDesarrollo(course).subscribe()
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.courseService.addCourse({ name } as Course).subscribe(
+      course => {
+        this.courses.push(course);
+      }
+    )
+  }
+
+}
